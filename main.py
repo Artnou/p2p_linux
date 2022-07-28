03f199b82b06571f72c780551046e534e85cc1e6
Arthur Pourrier <arthur.pourrier@gmail.com>
2022-07-28 17:53:50 +0100

    Add imports; first socket and input

---
+++ main.py
@@ -0,0 +1,13 @@
+import socket
+import threading
+
+PORT = 5555
+
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+    s.bind(('0.0.0.0', PORT))
+
+    print("-- Client launched --")
+
+    send_ip = input('Send message to (ip address): ')
+
+    print('Send to', send_ip)
