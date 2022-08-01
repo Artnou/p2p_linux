d1275f9d3cc0cf24d8e96a8b73329181d8b60192
Arthur Pourrier <arthur.pourrier@gmail.com>
2022-08-01 17:04:40 +0100

    Add listen thread

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import socket
+import sys
 import threading
 
 PORT = 5555
@@ -10,12 +11,31 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
 
     print('-- Client launched --')
 
+    def listen():
+        while True:
+            data, addr = s.recvfrom(1024)
+            data = data.decode()
+            recv_ip, recv_port = addr
+            print('\r{} --> You: {}'.format(recv_ip, data))
+            print('You --> {}'.format(send_ip))
+
+    listener = threading.Thread(target=listen, daemon=True)
+    listener.start()
+
     while True:
         send_ip = input('Send message to (ip address): ')
-        msg = input('To {} > '.format(send_ip))
 
-        s.sendto(msg.encode(), (send_ip, PORT))
+        if send_ip == 'exit':
+            sys.exit()
+        
+        while True:
+            msg = input('You --> {}: '.format(send_ip))
 
-        print('You --> {}: {}'.format(send_ip, msg))
+            if msg == 'change':
+                break
+            elif msg == 'exit':
+                sys.exit()
+
+            s.sendto(msg.encode(), (send_ip, PORT))
     
     
