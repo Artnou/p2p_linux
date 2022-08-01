df5d72d4482437f7b4bd6e02d725fce0e6beca15
Arthur Pourrier <arthur.pourrier@gmail.com>
2022-08-01 15:50:00 +0100

    Add debug file to check reception

---
+++ receive.py
@@ -0,0 +1,17 @@
+import socket
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--ip", nargs='?', type=str)
+args = parser.parse_args()
+
+HOST_IP = args.ip
+
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+    s.bind((HOST_IP, 5555))
+
+    while True:
+        data, recv_ip = s.recvfrom(1024)
+        data = data.decode()
+
+        print("Message received from {}: {}".format(recv_ip, data))
