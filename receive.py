d802ca2e80b81aea7ce210a7ee5cd587dbdc9ba9
Arthur Pourrier <arthur.pourrier@gmail.com>
2022-08-01 16:17:51 +0100

    Modified receive UI

--- receive.py
+++ receive.py
@@ -1,20 +1,22 @@
 import socket
 import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--ip", nargs='?', type=str)
-args = parser.parse_args()
+# parser = argparse.ArgumentParser()
+# parser.add_argument("--ip", nargs='?', type=str)
+# args = parser.parse_args()
+# 
+# HOST_IP = args.ip
 
-HOST_IP = args.ip
+HOST_IP = '0.0.0.0'
 
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
     s.bind((HOST_IP, 5555))
 
     print("-- Reception debug launched --")
-    print("Receiver ip:", HOST_IP)
 
     while True:
-        data, recv_ip = s.recvfrom(1024)
+        data, addr = s.recvfrom(1024)
         data = data.decode()
+        recv_ip, recv_port = addr
 
-        print("Message received from {}: {}".format(recv_ip, data))
+        print("{} --> You: {}".format(recv_ip, data))
