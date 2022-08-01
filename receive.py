527e3d9755d5e5349e81178cc98fe57383e518b2
Arthur Pourrier <arthur.pourrier@gmail.com>
2022-08-01 15:55:37 +0100

    Print ip of receiver

--- receive.py
+++ receive.py
@@ -10,6 +10,9 @@ HOST_IP = args.ip
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
     s.bind((HOST_IP, 5555))
 
+    print("-- Reception debug launched --")
+    print("Receiver ip:", HOST_IP)
+
     while True:
         data, recv_ip = s.recvfrom(1024)
         data = data.decode()
