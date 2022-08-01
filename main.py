5b502523898a32ba212320b50b17b76a819d3914
Arthur Pourrier <arthur.pourrier@gmail.com>
2022-08-01 15:38:30 +0100

    Add ip as argument

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
 import socket
 import threading
+import argparse
 
+parser = argparse.ArgumentParser()
+parser.add_argument("--ip", nargs='?', type=str)
+args = parser.parse_args()
+
+HOST_IP = args.ip
+# HOST_IP = socket.gethostbyname_ex(socket.getfqdn())
 PORT = 5555
 
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
     s.bind(('0.0.0.0', PORT))
 
+    # host_ip = socket.gethostbyname(socket.gethostname())
+
     print("-- Client launched --")
+    print("Host ip:", HOST_IP)
 
     send_ip = input('Send message to (ip address): ')
 
     print('Send to', send_ip)
 
-    # test 8
-    # test 9
-    # test 10
+    s.sendto("Message from".encode(), (send_ip, PORT))
+    
+    
