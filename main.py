4b5e961382089d430ac10b42204107488d435e6f
Arthur Pourrier <arthur.pourrier@gmail.com>
2022-08-09 12:34:40 +0100

    Comment messaging and add peers list sharing

--- main.py
+++ main.py
@@ -6,15 +6,22 @@ import re
 
 PORT = 5555
 
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+    try:
+        s.connect(('10.255.255.255', 1))
+        IP = s.getsockname()[0]
+    except Exception:
+        IP = '127.0.0.1'
+    finally:
+        s.close
+
+print('User ip: {}'.format(IP))
+
 peers = []
-colors = ['red','green','yellow','cyan','magenta','blue']
+peers.append(IP)
 
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
     s.bind(('0.0.0.0', PORT))
-
-    send_ip = '0.0.0.0'
-
-    print(colored('-- Client launched --', 'grey', 'on_white'))
 
     def listen():
         while True:
@@ -24,37 +31,66 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
 
             if recv_ip not in peers:
                 peers.append(recv_ip)
+                msg = ''
 
-            color = colors[peers.index(recv_ip)]
+                for peer in peers:
+                    if peers.index(peer) == 0:
+                        msg = peer
+                    else:
+                        msg = msg + ' ' + peer
 
-            if send_ip == '0.0.0.0':
-                print(colored('\r{} --> You: '.format(recv_ip), color) + '{}\nSend to: '.format(data), end='')
-            else:
-                print(colored('\r{} --> You: '.format(recv_ip), color) + '{}\nYou --> {}: '.format(data, send_ip), end='')
+                for peer in peers:
+                    s.sendto(msg.encode(), (peer, PORT))
 
-    listener = threading.Thread(target=listen, daemon=True)
-    listener.start()
+# Old peer-to-peer communication
 
-    while True:
-        while True:
-            send_ip = input('Send to: ')
-
-            if send_ip == '/exit':
-                sys.exit()
-
-            if re.match(r"192+\.+168+\.+5+\.+\b([01]?[0-9][0-9]?|2[0-4][0-9]|25[0-5])$", send_ip):
-                break
-            else:
-                print(colored('error: invalid ip address', 'white', 'on_red'))
-        
-        while True:
-            msg = input('You --> {}: '.format(send_ip))
-
-            if msg == '/change':
-                send_ip = '0.0.0.0'
-                break
-            elif msg == '/exit':
-                sys.exit()
-
-            s.sendto(msg.encode(), (send_ip, PORT))
-       
+#colors = ['red','green','yellow','cyan','magenta','blue']
+#
+#with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+#    s.bind(('0.0.0.0', PORT))
+#
+#    send_ip = '0.0.0.0'
+#
+#    print(colored('-- Client launched --', 'grey', 'on_white'))
+#
+#    def listen():
+#        while True:
+#            data, addr = s.recvfrom(1024)
+#            data = data.decode()
+#            recv_ip, recv_port = addr
+#
+#            if recv_ip not in peers:
+#                peers.append(recv_ip)
+#
+#            color = colors[peers.index(recv_ip)]
+#
+#            if send_ip == '0.0.0.0':
+#                print(colored('\r{} --> You: '.format(recv_ip), color) + '{}\nSend to: '.format(data), end='')
+#            else:
+#                print(colored('\r{} --> You: '.format(recv_ip), color) + '{}\nYou --> {}: '.format(data, send_ip), end='')
+#
+#    listener = threading.Thread(target=listen, daemon=True)
+#    listener.start()
+#
+#    while True:
+#        while True:
+#            send_ip = input('Send to: ')
+#
+#            if send_ip == '/exit':
+#                sys.exit()
+#
+#            if re.match(r"192+\.+168+\.+5+\.+\b([01]?[0-9][0-9]?|2[0-4][0-9]|25[0-5])$", send_ip):
+#                break
+#            else:
+#                print(colored('error: invalid ip address', 'white', 'on_red'))
+#        
+#        while True:
+#            msg = input('You --> {}: '.format(send_ip))
+#
+#            if msg == '/change':
+#                send_ip = '0.0.0.0'
+#                break
+#            elif msg == '/exit':
+#                sys.exit()
+#
+#            s.sendto(msg.encode(), (send_ip, PORT))
